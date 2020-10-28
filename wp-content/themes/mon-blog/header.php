07563656f9dc94bffdb7be38753a9dd78c0fcb1b
Nicolas-Szelendar <7780SOni>
2020-10-28 17:32:14 +0100

    poste-thumbnails

--- wp-content/themes/mon-blog/header.php
+++ wp-content/themes/mon-blog/header.php
@@ -1,32 +1,35 @@
-
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <title><?php bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
-</head>
-<body>
-<header class="header">
-        <nav>
-            <input type="checkbox" id="check">
-            <label for="check" class="checkbtn"><i class="fa fa-bars"></i></label>
-            <label class="logo"><a href="<?= home_url(); ?>">D'albi à Quebec <h4>le blog</h4>
 
-            </label>
-            <?php if ( has_custom_logo() ) : ?>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <title><?php bloginfo('name'); ?></title>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header class="header">
+    <nav>
+      <input type="checkbox" id="check">
+      <div class="navbar-brand" href="#">
+                <img class="logo" src="" alt="">
+      </div>
+      <label for="check" class="checkbtn"><i class="fa fa-bars"></i></label>
+      <label class="logo"><a href="<?= home_url(); ?>">D'albi à Quebec <h4>le blog</h4>
+
+      </label>
+      <?php if (has_custom_logo()) : ?>
 
         <div class="site-logo"><?php the_custom_logo(); ?></div>
       <?php endif; ?>
-      <?php wp_nav_menu(['theme_location' => 'header','container' => 'false']) ?>
-          
-            <!-- 
+      <?php wp_nav_menu(['theme_location' => 'header', 'container' => 'false']) ?>
+
+      <!-- 
               <ul>
                 <li><a class="active" href=#>Accueil</a></li>
                 <li><a href=#>Les news</a></li>
                 <li><a href=#>Nous contacter</a></li>
             </ul>-->
-        </nav>
-        
+    </nav>
--- wp-content/themes/mon-blog/index.php
+++ wp-content/themes/mon-blog/index.php
@@ -1,40 +1,29 @@
-<?php get_header()?>
-
-    <!----------------Les premier articles-------------->
-
-    <section></section>
-
+<?php get_header() ?>
+<!----------------Header-------------->
+<section></section>
 </header>
-    <main class="main">
-        <div class="main-title">Les news</div>
+<main class="main">
+    <div class="main-title">Les news</div>
 
-            <?php if ( have_posts()): ?>
-            <div class="row">
+    <?php if (have_posts()) : ?>
+        <div class="row">
 
-                <?php while ( have_posts() ) : the_post();  ?>
+            <?php while (have_posts()) : the_post();  ?>
                 <div class="col-sm-4">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card">
+                        <img class="<?php the_post_thumbnail('post_thumbnail') ?>" src="..." alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title"><?php the_title() ?></h5>
                             <small><?php the_time('F jS, Y'); ?></small>
-                            <p class="card-text"><?php the_content()?><p>
-                            <a href="<?php the_permalink()?>" class="btn btn-primary">Voir plus</a>
+                            <p class="card-text"><?php the_content() ?><p>
+                                    <a href="<?php the_permalink() ?>" class="btn btn-primary">Voir plus</a>
                         </div>
                     </div>
                 </div>
-                        <?php endwhile; ?>
-                    <?php else: ?>
-                <?php endif; ?>
-            </div>
-        
-    
-
-    </main>
-
-    <!-- ---------------Footer----------------->
-
-<script src="/js/app.js"></script>
-
-</html>
+            <?php endwhile; ?>
+        <?php else : ?>
+    <?php endif; ?>
+        </div>
+</main>
+<!-- ---------------Footer----------------->
 <?php get_footer(); ?>
