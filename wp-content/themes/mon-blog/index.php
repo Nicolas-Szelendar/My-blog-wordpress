77c995b23633fef51c167ba27c5e637855bda45a
Nicolas-Szelendar <7780SOni>
2020-11-18 14:25:11 +0100

    install node_modules_boostrap

--- wp-content/themes/mon-blog/index.php
+++ wp-content/themes/mon-blog/index.php
@@ -3,30 +3,37 @@
 <section></section>
 </header>
 <main class="main">
-    <div class="main-title">Les news</div>
-
+    <div class="main-title">Les derniers articles</div>
+    
+    <div class="container">
+    <div class="row">
     <?php if (have_posts()) : ?>
-        <div class="card-deck">
+      <!-- Blog Entries Column -->
+      <?php while (have_posts()) : the_post();  ?>
+      <div class="col-md-8">
 
-            <?php while (have_posts()) : the_post();  ?>
-                <div class="col-sm-4">
-                    <div class="w-80 p-8">
-                        <div class="card">
-                        
-                    <?php the_post_thumbnail ('post-thumbnail', ['class' => 'card-img-top','alt' => 'style=width:100%; heith:50%;']); ?>
-                            <div class="card-body">
-                                <h5 class="card-title"><?php the_title() ?></h5>
-                                <h6>Publié le <?php the_time(get_option( 'date_format' )); ?></h6>
-                                <p class="card-text"><?php the_excerpt() ?><p>
-                                        <a href="<?php the_permalink() ?>" class="btn btn-primary">Voir plus</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <h1 class="my-4"><?php the_title() ?></h1>
+
+        <!-- Blog Post -->
+        <div class="card mb-4">
+          <?php the_post_thumbnail ('post-thumbnail', ['class' => 'card-img-top','alt' => 'style=width:100%; heith:50%;']); ?>
+          <div class="card-body">
+            <h2 class="card-title"><?php the_title() ?></h2>
+            <p class="card-text"><?php the_excerpt() ?></p>
+            <a href="<?php the_permalink() ?>" class="btn btn-primary">Voir plus &rarr;</a>
+          </div>
+          <div class="card-footer text-muted">
+            <p>Le<?php the_time(get_option( 'date_format' )); ?></p>
             <?php endwhile; ?>
         <?php else : ?>
     <?php endif; ?>
+          </div>
         </div>
+
+
+    
 </main>
+
+
 <!-- ---------------Footer----------------->
 <?php get_footer(); ?>
